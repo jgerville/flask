95e0dfe63592862c66e7fae4bca0cedebe9e539a
Julian Erville <jerville@gmail.com>
2022-01-14 08:52:39 -0500

    Prove that static routes take priority over dynamic routes

--- app.py
+++ app.py
@@ -8,7 +8,11 @@ def hello():
 
 @app.route("/<my_name>")
 def greet(my_name):
-  return "<h2>sup {}</h2>".format(my_name)
+  return "<h2>{}, did you know about the recursion joke?</h2>".format(my_name) * 20
+
+@app.route("/test")
+def figure_out():
+  return "do we get this or the dynamic route?"
 
 if __name__ == "__main__":
   app.run(port=5000, debug=True)
