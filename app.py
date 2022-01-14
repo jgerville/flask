005320d952115acb0e292ad47c19fe36bfd3f107
Julian Erville <jerville@gmail.com>
2022-01-14 08:44:05 -0500

    Add dynamic route

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  return "<h1>heyyyyy</h1>";
+  return "<h1>heyyyyy</h1>"
+
+@app.route("/<my_name>")
+def greet(my_name):
+  return "<h2>sup {}</h2>".format(my_name)
 
 if __name__ == "__main__":
   app.run(port=5000, debug=True)
