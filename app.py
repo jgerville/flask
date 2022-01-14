92052a627abf72b66fd5d92be68f048795574455
Julian Erville <jerville@gmail.com>
2022-01-14 10:10:07 -0500

    Add home and about static templates; render templates

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def home():
-  return "Learn to play Go"
+  return render_template("home.html")
 
 @app.route("/about")
 def about():
-  return "This is a tiny little website that I'm creating in order to get familiar with Flask!"
+  return render_template("about.html")
 
 @app.route("/<other>")
 def catchall(other):
